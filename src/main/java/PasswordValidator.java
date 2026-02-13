84ee1c522979094f37c06bf5a2f8cc4c99b76525
jordyH54 <jordy_hdez@hotmail.com>
2026-02-13 12:56:38 -0600

    se realiza prueba de test validacion de password se pasan todas las pruebas

--- src/main/java/PasswordValidator.java
+++ src/main/java/PasswordValidator.java
@@ -1,3 +1,12 @@
 public class PasswordValidator {
 
+    public boolean isValid(String password) {
+
+        if (password == null) {
+            return false;
+        }
+
+        String regex = "^(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z0-9]).{6,}$";
+        return password.matches(regex);
+    }
 }
